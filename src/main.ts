430a1c156baf8d9b7943d7b6549cb15ca618915c
Antonio Dias <antonio@cubotecnologiabr.com.br>
2025-12-04 16:34:30 -0300

    [fix][main] - adjusting cors config

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.setGlobalPrefix('/api');
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
     credentials: true,
   });
 
